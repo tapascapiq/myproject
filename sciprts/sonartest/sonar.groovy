0c156ca1411fdbe2c7257efd9f46181e8147fd57
tkdalai <Tapas.Dalai@narisk.com>
2025-08-24 18:01:22 +0530

    sonar

--- sciprts/sonartest/sonar.groovy
+++ sciprts/sonartest/sonar.groovy
@@ -2,49 +2,30 @@ pipeline {
     agent any
 
     tools {
-        // Only if you want Jenkins to use installed tools
-        // sonarQubeScanner 'SonarScanner'
+        // Use the name you configured in "Global Tool Configuration"
+        sonarQubeScanner 'MySonar'
     }
 
     stages {
-        stage('Checkout') {
-            steps {
-                git branch: 'main', url: 'https://github.com/tapascapiq/myproject.git'
-            }
-        }
-
-        stage('Build') {
-            steps {
-                echo "Building the project..."
-                // add build commands
-            }
-        }
-
         stage('SonarQube Analysis') {
             steps {
-                withSonarQubeEnv('MySonar') { // 'MySonar' = name you gave in Jenkins config
-                    sh """
+                withSonarQubeEnv('sonarscanner') {  // name from Manage Jenkins > Configure System
+                    sh '''
                         sonar-scanner \
-                          -Dsonar.projectKey=myproject \
-                          -Dsonar.sources=. \
-                          -Dsonar.host.url=http://localhost:9000 \
-                          -Dsonar.login=$SONAR_AUTH_TOKEN
-                    """
+                        -Dsonar.projectKey=myproject \
+                        -Dsonar.sources=. \
+                        -Dsonar.host.url=http://localhost:9000 \
+                        -Dsonar.login=<your-token>
+                    '''
                 }
             }
         }
 
         stage('Quality Gate') {
             steps {
-                timeout(time: 1, unit: 'HOURS') { // waits until analysis is done
+                timeout(time: 1, unit: 'MINUTES') {
                     waitForQualityGate abortPipeline: true
                 }
-            }
-        }
-
-        stage('Deploy') {
-            steps {
-                echo "Deploying..."
             }
         }
     }
